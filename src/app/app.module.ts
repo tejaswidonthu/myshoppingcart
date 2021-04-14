1985a5e63ebd1450538c6792856753a590049ecd
TEJASWI <TEJASHARATH12@GMAIL.COM>
2021-04-14 12:25:08 +0200

    init

---
+++ src/app/app.module.ts
@@ -0,0 +1,31 @@
+import { NgModule } from '@angular/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { CategoryComponent } from './category/category.component';
+import { ProductListComponent } from './productList/productList.component';
+import { ProductItemComponent } from './productList/productItem/productItem.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    CategoryComponent,
+    ProductListComponent,
+    ProductItemComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    ReactiveFormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ src/app/productList/productItem/productItem.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-productitem',
+  templateUrl: './productItem.component.html',
+  styleUrls: ['./productItem.component.css']
+})
+export class ProductItemComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
